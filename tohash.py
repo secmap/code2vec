03c595b81d672e2280323d3c7edfb9d0a22a8572
BDSword <thetruebloodsword@gmail.com>
2017-07-18 13:38:18 +0800

    Fix single file tohash no output problem.

--- tohash.py
+++ tohash.py
@@ -9,21 +9,24 @@ import hashlib
 import re
 
 USAGE = "Transfer binary file to per instruction hashes\nUsage: {} <binary or search-root-folder> <output-filename>"
-ERROR = ["{} is not a PE file.", "objdump error."]
+ERROR = ["{} is not a PE file.", "objdump error.", "Obj2hash error."]
 
 CONSTANT_SYM = r'{const}'
+
 
 class Obj2hash():
 
     def __init__(self, h_size=13):
         self.h_size = 2**h_size
-        self.hash_fn = lambda str: int(hashlib.md5(str.encode('utf-8')).hexdigest(), 16)% self.h_size
+        self.hash_fn = lambda str: int(hashlib.md5(
+            str.encode('utf-8')).hexdigest(), 16) % self.h_size
         self.h_table = [set() for i in range(self.h_size)]
         self.total = 0
         self.c_count = 0
 
     def obj2hash(self, file):
-        output = subprocess.check_output(['objdump', '-M', 'intel', '-d', file])
+        output = subprocess.check_output(
+            ['objdump', '-M', 'intel', '-d', file])
         if output == None:
             print(ERROR[1], file=sys.stderr)
             return None
@@ -37,14 +40,16 @@ class Obj2hash():
                 del output[i]
             else:
                 output[i] = output[i].split("\t")[2]
+                output[i].replace(' ', '')
                 tmp = output[i]
                 output[i] = re.sub(r'\-(0x[\dabcdef]+)', r'+\1', output[i])
                 output[i] = re.sub(r'\-(\d+)', r'+\1', output[i])
-                output[i] = re.sub(r'(?!^)\b((0x)?)[0-9a-f]+\b', CONSTANT_SYM, output[i])
+                output[i] = re.sub(
+                    r'(?!^)\b((0x)?)[0-9a-f]+\b', CONSTANT_SYM, output[i])
                 output[i] = re.sub(r'(?!^)\b\d+\b', CONSTANT_SYM, output[i])
                 output[i] = re.sub(r'<.*>$', r'', output[i])
                 output[i] = re.sub(r'#.*$', r'', output[i])
-                
+
         hash_list = []
         for i in output:
             h = self.hash_fn(i)
@@ -58,7 +63,7 @@ class Obj2hash():
 
     def collision(self):
         print("{} {}".format(self.c_count, self.total))
-        #print("{} occur in size {} hash table.".format(count, len(self.h_table)))
+        # print("{} occur in size {} hash table.".format(count, len(self.h_table)))
 
     def save_table(self, file):
         with open(file, 'wb') as output:
@@ -107,10 +112,17 @@ def main():
             sys.exit(1)
 
         tohash = Obj2hash(22)
-        tohash.obj2hash(file)
-        tohash.save_table('hash.plk')
-        #tohash.load_table('hash.plk')
-        tohash.collision()
+        hash_list = tohash.obj2hash(file)
+        if hash_list is not None:
+            hash_list = [str(h) for h in hash_list]
+            hash_str = ' '.join(hash_list)
+            output_file.write(hash_str + '\n')
+            tohash.save_table('hash.plk')
+            # tohash.load_table('hash.plk')
+            tohash.collision()
+        else:
+            print(ERROR[2], file=sys.stderr)
+            sys.exit(1)
 
 
 if __name__ == "__main__":
