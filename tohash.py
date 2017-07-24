02e8689585f015a2f4de81de63c6f0c16b7cd61f
BDSword <thetruebloodsword@gmail.com>
2017-07-24 16:15:53 +0800

    Apply bloom filter to english words.

--- tohash.py
+++ tohash.py
@@ -9,11 +9,8 @@ import hashlib
 import re
 from bf import bloomfilter
 
-USAGE = "Transfer binary file to per instruction hashes\nUsage: {} <binary or search-root-folder> <output-filename>"
-ERROR = ["{} is not a PE file.", "objdump error.", "Obj2hash error."]
-
-CONSTANT_SYM = r'{const}'
-
+USAGE = "Transfer text file to per instruction hashes\nUsage: {} <input text file or root folder> <output-filename>"
+ERROR = ['Read file error']
 
 class Obj2hash():
 
@@ -21,22 +18,12 @@ class Obj2hash():
         self.bloomfilter = bloomfilter(name=name)
 
     def obj2hash(self, file):
-        output = subprocess.check_output(
-            ['objdump', '-M', 'intel', '-d', file])
+        output = open(file, 'r').read()
         if output == None:
             print(ERROR[1], file=sys.stderr)
             return None
 
-        output = output.decode('utf-8', 'ignore')
-        output = output.split("\n")
-        for i in range(len(output) - 1, -1, -1):
-            if output[i] == '':
-                del output[i]
-            elif output[i].count("\t") < 2:
-                del output[i]
-            else:
-                output[i] = output[i].split("\t")[1]
-                output[i] = output[i].replace(' ', '')
+        output = output.split()
 
         hash_list = []
         for i in output:
@@ -47,12 +34,6 @@ class Obj2hash():
 
     def save_table(self):
         self.bloomfilter.save()
-
-
-def is_valid_header(header):
-    if 'PE' in header and 'executable' in header:
-        return True
-    return False
 
 
 def main():
@@ -72,25 +53,15 @@ def main():
                     file_path = os.path.join(root, filename)
                     file_size = os.path.getsize(file_path)
                     print('Processing: {}'.format(file_path))
-                    try:
-                        if is_valid_header(magic.Magic().id_filename(file_path)) and file_size < 5242880:
-                            hash_list = tohash.obj2hash(file_path)
-                            if hash_list is not None:
-                                hash_list = [str(h) for h in hash_list]
-                                hash_str = ' '.join(hash_list)
-                                output_file.write(hash_str + '\n')
-                    except UnicodeEncodeError:
-                        pass
-                    except subprocess.CalledProcessError:
-                        print('Unable to process {}'.format(file_path))
+                    hash_list = tohash.obj2hash(file_path)
+                    if hash_list is not None:
+                        hash_list = [str(h) for h in hash_list]
+                        hash_str = ' '.join(hash_list)
+                        output_file.write(hash_str + '\n')
         finally:
             tohash.save_table()
             output_file.close()
     else:
-        if not is_valid_header(magic.Magic().id_filename(file)):
-            print(ERROR[0].format(sys.argv[1]), file=sys.stderr)
-            sys.exit(1)
-
         tohash = Obj2hash(sys.argv[2])
         hash_list = tohash.obj2hash(file)
         if hash_list is not None:
@@ -99,7 +70,6 @@ def main():
             output_file.write(hash_str + '\n')
             tohash.save_table()
         else:
-            print(ERROR[2], file=sys.stderr)
             sys.exit(1)
         output_file.close()
 
