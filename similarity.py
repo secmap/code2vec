809dfc1823083f2ba56146ab4c1cd6cd75d29aba
BDSword <thetruebloodsword@gmail.com>
2017-07-25 11:52:30 +0800

    Apply similarity.py to origin word2vec.

--- similarity.py
+++ similarity.py
@@ -4,44 +4,53 @@ import tensorflow as tf
 import sys
 import os
 import numpy as np
-import bf
-import pwn
+import collections
 
-if len(sys.argv) != 4:
-    print('Usage:\n\tsimilarity.py <input filename> <bloom filter plk> <model filename>')
+
+if len(sys.argv) != 3:
+    print('Usage:\n\tsimilarity.py <input filename> <model filename>')
     sys.exit(-1)
 
-print('Loading bloomfilter...', end='')
-bloomfilter = bf.bloomfilter()
-bloomfilter.load(sys.argv[2])
-print('Done')
-
-def get_word_indice(word):
-    indice = bloomfilter.get_indice(word)
-    return indice
-
-
 def read_data(filename):
-    """Extract the first file enclosed in a zip file as a list of words."""
-    with open(filename) as f:
-        filter_set = set()
-        unsorted_res = []
-        data = tf.compat.as_str(f.read()).split()
-        for word in data:
-            word_idx_list = [int(idx) for idx in word.split(',')]
-            filter_set.add(tuple(word_idx_list))
-        for w in filter_set:
-            unsorted_res.append(list(w))
-    return unsorted_res 
+  """Extract the first file enclosed in a zip file as a list of words."""
+  data = open(filename, 'r').read().split()
+  return data
 
-print('Read vocabulary from {}...'.format(sys.argv[1]), end='')
+print('Read input file from {}...'.format(sys.argv[1]), end='')
 vocabulary = read_data(sys.argv[1])
 print('Done')
 
-embedding_size = 128  # Dimension of the embedding vector.
+# Step 2: Build the dictionary and replace rare words with UNK token.
+vocabulary_size = 50000
 
-bloom_filter_max_size = 65536
-num_hash_fun = 7
+
+def build_dataset(words, n_words):
+  """Process raw inputs into a dataset."""
+  count = [['UNK', -1]]
+  count.extend(collections.Counter(words).most_common(n_words - 1))
+  dictionary = dict()
+  for word, _ in count:
+    dictionary[word] = len(dictionary)
+  data = list()
+  unk_count = 0
+  for word in words:
+    if word in dictionary:
+      index = dictionary[word]
+    else:
+      index = 0  # dictionary['UNK']
+      unk_count += 1
+    data.append(index)
+  count[0][1] = unk_count
+  reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+  return data, count, dictionary, reversed_dictionary
+
+print('Build dataset...', end='')
+data, count, dictionary, reverse_dictionary = build_dataset(vocabulary,
+                                                            vocabulary_size)
+del vocabulary  # Hint to reduce memory.
+print('Done')
+
+embedding_size = 128  # Dimension of the embedding vector.
 
 top_k = 8
 
@@ -50,29 +59,25 @@ print('Construct required tf graph...', end='')
 graph = tf.Graph()
 
 with graph.as_default():
-    test_input = tf.placeholder(tf.int32, shape=[num_hash_fun])
+    test_input = tf.placeholder(tf.int32, shape=[1])
 
     # Ops and variables pinned to the CPU because of missing GPU implementation
     with tf.device('/cpu:0'):
         embeddings = tf.Variable(
-            tf.random_uniform([bloom_filter_max_size, embedding_size], -1.0, 1.0))
+                        tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
 
     # Compute the average NCE loss for the batch.
     # tf.nce_loss automatically draws a new sample of the negative labels each
     # time we evaluate the loss.
     test_vec = tf.nn.embedding_lookup(embeddings, test_input)
-    test_vec = tf.reduce_mean(test_vec, 0)
-    test_vec = tf.expand_dims(test_vec, 0)
     print('test_vec.shape: ', test_vec.shape)
 
 
     # Compute the cosine similarity between minibatch examples and all embeddings.
     norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
     normalized_embeddings = embeddings / norm
-    all_words_embeddings = tf.nn.embedding_lookup(normalized_embeddings, vocabulary)
-    all_words_embeddings = tf.reduce_mean(all_words_embeddings, 1)
 
-    similarity = tf.matmul(test_vec, all_words_embeddings, transpose_b=True)
+    similarity = tf.matmul(test_vec, normalized_embeddings, transpose_b=True)
 
     # Add variable initializer.
     init = tf.global_variables_initializer()
@@ -84,36 +89,25 @@ with tf.Session(graph=graph) as session:
     # We must initialize all variables before we use them.
     init.run()
 
-    print('Restore embeddings weights from model({})...'.format(sys.argv[3]), end='')
+    print('Restore embeddings weights from model({})...'.format(sys.argv[2]), end='')
     saver = tf.train.Saver({'embeddings': embeddings})
-    saver.restore(session, sys.argv[3])
+    saver.restore(session, sys.argv[2])
     print('Done')
     
     while True:
-        raw_user_input = input('Please input a word (idx1,idx2,...idx7) or word or exit:')
+        raw_user_input = input('Please input a word or exit:')
         if raw_user_input == 'exit':
             break
-        if raw_user_input.startswith('('):
-            user_input = [int(i) for i in raw_user_input[1:-1].split(',')]
-        else:
-            user_input = get_word_indice(raw_user_input)
+            
+        user_input = [dictionary[raw_user_input]]
 
         feed_dict = {test_input: user_input}
         sim = session.run([similarity], feed_dict=feed_dict)[0][0]
         nearest = (-sim).argsort()[1:top_k + 1]
         log_str = 'Nearest to <{}>:'.format(raw_user_input)
+        close_words = []
         for k in range(top_k): # Iterate each top_k closed word
-            close_opcode_indice = vocabulary[nearest[k]] # all the hash value of the closed word
-            opcode_str = ''
-            possible_words = set()
-            for idx, val in enumerate(close_opcode_indice):
-                if idx == 0:
-                    possible_words = bloomfilter.get_opcode_in_table(idx, val)
-                else:
-                    possible_words &= bloomfilter.get_opcode_in_table(idx, val)
-            # opcode_asm = pwn.disasm(opcode)
-            if len(possible_words) == 0:
-                print('Unable to find reversed opcode for: {}'.format(close_opcode_indice))
-            else:
-                print(possible_words)
+            close_word = reverse_dictionary[nearest[k]] # all the hash value of the closed word
+            close_words.append(close_word)
+        print(', '.join(close_words))
         print('=' * 80)
