210f4c8369d9e363adb2205b1ba910aab0c5c07c
BDSword <thetruebloodsword@gmail.com>
2017-07-27 15:24:25 +0800

    Add count_frequency.py.

--- count_frequency.py
+++ count_frequency.py
@@ -4,39 +4,47 @@ import sys
 import bf
 import collections
 import tensorflow as tf
-import pwn
 import re
 
 if len(sys.argv) != 4:
     print('Usage:\n\tcount_frequency.py <input filename> <bloom filter plk> <output filename>')
     sys.exit(-1)
 
-def get_words_counter(words, num_of_most_common_word):
-    """Process raw inputs into a dataset."""
 
-    words_counter = collections.Counter(words).most_common(num_of_most_common_word)
-    return words_counter
-
-
-def get_asm_indice(asm_str):
-    opcode = pwn.asm(asm_str)
-    indice = bloomfilter.get_indice(opcode)
-    return indice
-
-
-def read_data(filename):
+def read_data(filename, n_words):
     """Extract the first file enclosed in a zip file as a list of words."""
     with open(filename) as f:
-        res = []
-        data = tf.compat.as_str(f.read()).split()
-        for word in data:
+        filter_set = set()
+        unsorted_res = []
+        words = []
+        count = []
+        for line in f:
+            word = line.strip()
+            if len(word) == 0:
+                continue
             word_idx_list = [int(idx) for idx in word.split(',')]
-            res.append(tuple(word_idx_list))
-    return res
+            filter_set.add(tuple(word_idx_list))
+            words.append(tuple(sorted(word_idx_list)))
+        words_counter = collections.Counter(words)
+        most_common_words = dict()
+        most_common_words_counter = words_counter.most_common(n_words)
+        for item in most_common_words_counter:
+            most_common_words[item[0]] = item[1]
 
+        for w in filter_set:
+            if tuple(sorted(list(w))) in most_common_words:
+                unsorted_res.append(list(w))
+    
+    del words
+    del count
+    del filter_set
+
+    return unsorted_res, most_common_words
+
+n_words = 50000
 
 print('Read vocabulary from {}...'.format(sys.argv[1]), end='')
-vocabulary = read_data(sys.argv[1])
+vocabulary, most_common_words = read_data(sys.argv[1], n_words)
 print('Done')
 
 print('Loading bloomfilter...', end='')
@@ -46,26 +54,21 @@ print('Done')
 
 output_file = open(sys.argv[3], 'w')
 
-counter = get_words_counter(vocabulary, 1000)
+for item in vocabulary:
+    hash_indice = tuple(sorted(list(item)))
+    count = most_common_words[hash_indice]
 
-for item in counter:
-    opcode_indice = list(item[0])
-    count = item[1]
-    opcodes = set()
-    for idx, val in enumerate(opcode_indice):
+    possible_words = set()
+    for idx, val in enumerate(hash_indice):
         if idx == 0:
-            opcodes = bloomfilter.get_opcode_in_table(idx, val)
+            possible_words = bloomfilter.get_opcode_in_table(idx, val)
         else:
-            opcodes &= bloomfilter.get_opcode_in_table(idx, val)
-    # opcode_asm = pwn.disasm(opcode)
-    if len(opcodes) == 0:
-        print('Unable to find reversed opcode for: {}'.format(opcode_indice))
+            possible_words &= bloomfilter.get_opcode_in_table(idx, val)
+            
+    if len(possible_words) == 0:
+        print('Unable to find reversed words for: {}'.format(item))
     else:
-        opcode_asm_list = []
-        for opcode in opcodes:
-            opcode_asm = pwn.disasm(bytearray.fromhex(opcode))
-            opcode_asm_list.append(opcode_asm)
-        output_file.write('\t#{}\t{}\n'.format(count, '; '.join(opcode_asm_list)))
+        output_file.write('{}\t#{}\t{}\n'.format(possible_words, item, count))
 
 output_file.close()
     
