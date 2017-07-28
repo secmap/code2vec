44c23eb6fe7c8bd95d357747458b3b6751e52713
BDSword <thetruebloodsword@gmail.com>
2017-07-28 16:21:06 +0800

    Fix read data in count_frequency.py.

--- count_frequency.py
+++ count_frequency.py
@@ -31,20 +31,25 @@ def read_data(filename, n_words):
         for item in most_common_words_counter:
             most_common_words[item[0]] = item[1]
 
+        unsorted_res = dict()
         for w in filter_set:
-            if tuple(sorted(list(w))) in most_common_words:
-                unsorted_res.append(list(w))
+            if w == is_tuple:
+                print('Found')
+            sorted_tuple = tuple(sorted(list(w)))
+            if sorted_tuple in most_common_words:
+                unsorted_res[w] = most_common_words[sorted_tuple]
     
     del words
     del count
     del filter_set
+    del most_common_words
 
-    return unsorted_res, most_common_words
+    return unsorted_res
 
 n_words = 50000
 
 print('Read vocabulary from {}...'.format(sys.argv[1]), end='')
-vocabulary, most_common_words = read_data(sys.argv[1], n_words)
+most_common_words = read_data(sys.argv[1], n_words)
 print('Done')
 
 print('Loading bloomfilter...', end='')
@@ -54,21 +59,18 @@ print('Done')
 
 output_file = open(sys.argv[3], 'w')
 
-for item in vocabulary:
-    hash_indice = tuple(sorted(list(item)))
-    count = most_common_words[hash_indice]
-
+for word_tuple, word_count in most_common_words.items():
     possible_words = set()
-    for idx, val in enumerate(hash_indice):
+    for idx, val in enumerate(word_tuple):
         if idx == 0:
             possible_words = bloomfilter.get_opcode_in_table(idx, val)
         else:
-            possible_words &= bloomfilter.get_opcode_in_table(idx, val)
+            possible_words = possible_words & bloomfilter.get_opcode_in_table(idx, val)
             
     if len(possible_words) == 0:
-        print('Unable to find reversed words for: {}'.format(item))
+        print('Unable to find reversed words for: {}'.format(word_tuple))
     else:
-        output_file.write('{}\t#{}\t{}\n'.format(possible_words, item, count))
+        output_file.write('{}\t#{}\t{}\n'.format(possible_words, word_tuple, word_count))
 
 output_file.close()
     
